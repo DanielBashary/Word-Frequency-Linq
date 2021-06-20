c795a07e52cb2c790a28c88272c0f6208c09d5af
Daniel Bashary <dbashary@student.touro.edu>
2021-06-20 12:32:37 +0300

    DANIEL BASHARY

--- HelloGithubClassroom/Program.cs
+++ HelloGithubClassroom/Program.cs
@@ -1,12 +1,65 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using MoreLinq;
 
 namespace HelloGithubClassroom
 {
-    class Program
+    public static class Program
     {
-        static void Main(string[] args)
+        public static int DivisibleByThreeOrFive(int start, int end)
         {
-            Console.WriteLine("Hello World!");
+            return Enumerable.Range(start, end).Sum(x => x % 5 == 0 || x % 3 == 0 ? x : 0);
+        }
+
+        public static IEnumerable<KeyValuePair<string, int>> Top20Words(String book)
+        {
+            String StartOfFile = "C:\\Users\\Daniel Bashary\\source\\repos\\06-linq1-basharyd\\TXT\\";
+            return File.ReadAllLines(StartOfFile + book + ".txt")
+                .SkipUntil(x => x.ToUpper().Contains("START"))
+                .TakeUntil(x => x.ToUpper().Contains("GUTENBERG"))
+                .SelectMany(line => line.Split(' ', StringSplitOptions.RemoveEmptyEntries)) //todo check
+                .GroupBy(w => w.Any(char.IsDigit) ? "" : Regex.Replace(w, @"[^a-zA-Z0-9 -]", "").ToLower())
+                .Where(w => w.Key != "")
+                .ToDictionary(g => g.Key, g => g.Count())
+                .OrderByDescending(g => g.Value)
+                .Take(20); ;
+        }
+
+        public static String[,] PercentageCreatorAndPrinter()
+        {
+            List<String> bookNames = new List<string>()
+            {
+                "A Tale of Two Cities", "Deuteronomy", "Exodus",
+                "Genesis", "Hamlet", "Numbers", "Oliver Twist", "Romeo and Juliet", "Vayikra"
+            };
+            String[,] percentageCount = new String[11, 11];
+            for (int i = 0; i < bookNames.Count; i++)
+            {
+                percentageCount[i + 1, 0] = bookNames[i];
+                percentageCount[0, i + 1] = bookNames[i];
+                HashSet<String> topWords = new HashSet<string>(Program.Top20Words(bookNames[i])
+                    .Select(x => x.Key));
+                for (int j = 0; j < bookNames.Count; j++)
+                {
+                    if (i != j)
+                    {
+                        HashSet<String> check = new HashSet<string>(topWords);
+                        HashSet<String> topWords2 = new HashSet<string>(Program.Top20Words(bookNames[j])
+                            .Select(x => x.Key));
+                        check.UnionWith(topWords2);
+                        double amount = Math.Round(((20.0 - (check.Count - 20.0)) / 20.0) * 100.0);
+                        percentageCount[i + 1, j + 1] = amount + "";
+                    }
+                    else
+                    {
+                        percentageCount[i + 1, j + 1] = 100 + "";
+                    }
+                }
+            }
+            return percentageCount;
         }
     }
 }
---
+++ LinqTest/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MoreLinq;
+using HelloGithubClassroom;
+
+namespace LinqTest
+{
+    [TestClass]
+    public class LinqTest
+    {
+
+        [TestMethod]
+        public void PartOne()
+        {
+            Program.DivisibleByThreeOrFive(1, 9).Should().Be(23);
+        }
+
+
+
+        [TestMethod]
+        public void PartTwo()
+        {
+            int testSum = 0;
+            for (int i = 0; i < 1000; i++) { if (i % 5 == 0 || i % 3 == 0) { testSum += i; } }
+            Program.DivisibleByThreeOrFive(1, 999).Should().Be(testSum);
+        }
+
+        [TestMethod]
+        public void PartThreeTester()
+        {
+            String StartOfFile = "C:\\Users\\Daniel Bashary\\source\\repos\\06-linq1-basharyd\\TXT\\";
+            List<String> bookNames = new List<string>() {"A Tale of Two Cities", "Deuteronomy", "Exodus", 
+                "Genesis","Hamlet","Numbers","Oliver Twist", "Romeo and Juliet", "Vayikra"};
+            var checker = File.ReadAllLines(StartOfFile + "Hamlet" + ".txt")
+                .SkipUntil(x => x.ToUpper().Contains("START"))
+                .TakeUntil(x => x.ToUpper().Contains("GUTENBERG"))
+                .SelectMany(line => line.Split(' ', StringSplitOptions.RemoveEmptyEntries)) //todo check
+                //.TakeUntil(line => !line.Equals("end"))
+                .GroupBy(w => w.Any(char.IsDigit) ? "" : Regex.Replace(w, @"[^a-zA-Z0-9 -]", "").ToLower())
+                .Where(w => w.Key != "")
+                .ToDictionary(g => g.Key, g => g.Count())
+                .OrderByDescending(g => g.Value)
+                .Take(20);
+            checker.Select(i => $"{i.Key}: {i.Value}").ToList().ForEach(Console.WriteLine);
+        }
+        [TestMethod]
+        public void PartText()
+        {
+            List<String> bookNames = new List<string>() {"A Tale of Two Cities", "Deuteronomy", "Exodus",
+                "Genesis","Hamlet","Numbers","Oliver Twist", "Romeo and Juliet", "Vayikra"};
+            foreach (var book in bookNames)
+            {
+                Console.WriteLine("BOOK      " + book);
+                Console.WriteLine("--------------------------");
+                Program.Top20Words(book).Select(i => $"{i.Key}: {i.Value}").ToList().ForEach(Console.WriteLine);
+            }
+        }
+
+        [TestMethod]
+        public void PrintTableOfPercentages()
+        {
+            String[,] percentageCount = Program.PercentageCreatorAndPrinter();
+            var rowCount = percentageCount.GetLength(0);
+            var colCount = percentageCount.GetLength(1);
+            for (int row = 0; row < rowCount; row++)
+            {
+                for (int col = 0; col < colCount; col++)
+                    Console.Write(String.Format("{0}\t", percentageCount[row, col]));
+                Console.WriteLine();
+            }
+        }
+
+    }
+}
